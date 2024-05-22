2207271a2a1c61bbbbf8d4fe70c62128d6c44a9d
cinc-kominami <150752750+cinc-kominami@users.noreply.github.com>
2024-05-22 03:23:07 +0000

    km新規

--- km-new.js
+++ km-new.js
@@ -12,17 +12,14 @@
         const headers = {
             'Content-Type': 'application/json'
         };
-        return new kintone.Promise((resolve, reject) => {
-            kintone.proxy(webhookUrl, 'POST', {}, payload, (body, status, headers) => {
-                if (status >= 200 && status < 300) {
-                    console.log('Success:', status, body);
-                    resolve(e);
-                } else {
-                    console.error('Error:', status, body);
-                    reject(new Error('Webhook notification failed'));
-                }
-            });
+        kintone.proxy(webhookUrl, 'POST', headers, payload, (body, status, headers) => {
+            if (status >= 200 && status < 300) {
+                console.log('Success:', status, body);
+            } else {
+                console.error('Error:', status, body);
+            }
         });
+        return e;
     });
 
     /**
@@ -31,21 +28,19 @@
      * ・レコード一覧画面のインライン編集に成功したときのイベント
      */
     kintone.events.on(['app.record.edit.submit.success', 'mobile.app.record.edit.submit.success', 'app.record.index.edit.submit.success'], (e) => {
+
         const webhookUrl = 'https://hooks.zapier.com/hooks/catch/11728247/3vb2lh8/';
         const payload = e.record;
         const headers = {
             'Content-Type': 'application/json'
         };
-        return new kintone.Promise((resolve, reject) => {
-            kintone.proxy(webhookUrl, 'POST', {}, payload, (body, status, headers) => {
-                if (status >= 200 && status < 300) {
-                    console.log('Success:', status, body);
-                    resolve(e);
-                } else {
-                    console.error('Error:', status, body);
-                    reject(new Error('Webhook notification failed'));
-                }
-            });
+        kintone.proxy(webhookUrl, 'POST', headers, payload, (body, status, headers) => {
+            if (status >= 200 && status < 300) {
+                console.log('Success:', status, body);
+            } else {
+                console.error('Error:', status, body);
+            }
         });
+        return e;
     });
 })();
