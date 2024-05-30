aa1c077efe608b001cafa2511e19915b0f91f5a9
cinc-kominami <150752750+cinc-kominami@users.noreply.github.com>
2024-05-30 09:34:06 +0000

    add: consul-new

--- consul-new.js
+++ consul-new.js
@@ -6,6 +6,19 @@
      * ・レコード追加画面で保存に成功した後のイベント
      */
     kintone.events.on(['app.record.create.submit.success', 'mobile.app.record.create.submit.success'], (e) => {
+        
+        const webhookUrl = 'https://hooks.zapier.com/hooks/catch/11728247/2yh1xde/';
+        const payload = e.record;
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+        kintone.proxy(webhookUrl, 'POST', headers, payload, (body, status, headers) => {
+            if (status >= 200 && status < 300) {
+                console.log('Success:', status, body);
+            } else {
+                console.error('Error:', status, body);
+            }
+        });
         return e;
     });
 
